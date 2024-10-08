94bce8bf93b2c239c2b578400e9ba30613c02041
Glen Alloy <glenalloy8@gmail.com>
2024-10-08 19:55:18 +0530

    created fooditem component and mounted it in the food display component

--- food-delivery-front-end/src/components/foodDisplay/FoodDisplay.jsx
+++ food-delivery-front-end/src/components/foodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './foodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../foodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
 
@@ -11,7 +12,7 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=> {
-
+                return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
             })}
         </div>
     </div>
