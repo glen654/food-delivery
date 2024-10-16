1ae7bf4ac1c00bc7c2e7f0e33ae3a93f2b6ab4d4
Glen Alloy <glenalloy8@gmail.com>
2024-10-16 21:49:25 +0530

    implemented footer component

--- food-delivery-front-end/src/App.jsx
+++ food-delivery-front-end/src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Cart from './pages/cart/Cart'
 import PlaceOrder from './pages/place-order/PlaceOrder'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div className='app'>
-        <Navbar/>
+    <>
+      <div className='app'>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/order' element={<PlaceOrder/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
         </Routes>
-    </div>
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
---
+++ food-delivery-front-end/src/components/footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './footer.css'
+import {assets} from '../../assets/img/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" className='logo'/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta et veritatis aliquam facilis quia! Cumque aspernatur repudiandae exercitationem ab minus iste voluptates, delectus soluta accusamus earum ratione ducimus nemo eos?</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@yumrush.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+        <p className="footer-copyright">Copyright 2024 YumRush.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
