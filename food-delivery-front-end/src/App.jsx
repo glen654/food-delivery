3d0c214080b078e84241b5f23f58db414c856f29
Glen Alloy <glenalloy8@gmail.com>
2024-10-17 21:52:33 +0530

    show login state created

--- food-delivery-front-end/src/App.jsx
+++ food-delivery-front-end/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Cart from './pages/cart/Cart'
 import PlaceOrder from './pages/place-order/PlaceOrder'
 import Footer from './components/footer/Footer'
+import LoginPopUp from './components/loginPopUp/LoginPopUp'
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopUp/>:<></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
---
+++ food-delivery-front-end/src/components/loginPopUp/LoginPopUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loginPopUp.css'
+
+const LoginPopUp = () => {
+  return (
+    <div className='login-popup'>
+        <p>login</p>
+    </div>
+  )
+}
+
+export default LoginPopUp
