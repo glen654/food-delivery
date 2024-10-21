0ea958031686752679466c83b0e0a47c361554fe
Glen Alloy <glenalloy8@gmail.com>
2024-10-21 19:45:19 +0530

    implemented place order component

--- food-delivery-front-end/src/pages/place-order/PlaceOrder.jsx
+++ food-delivery-front-end/src/pages/place-order/PlaceOrder.jsx
@@ -3,7 +3,29 @@ import './placeOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First name'/>
+          <input type="text" placeholder='Last name'/>
+        </div>
+        <input type="text" placeholder='Email address'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='City'/>
+          <input type="text" placeholder='State'/>
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code'/>
+          <input type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone'/>
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
   )
 }
 
