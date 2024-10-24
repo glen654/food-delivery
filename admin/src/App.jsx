9b1ad08c84130087eaa71a57e6df14cab04f6739
Glen Alloy <glenalloy8@gmail.com>
2024-10-24 19:45:03 +0530

    created add list and orders components and implemented routes in the app jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
+import Add from './pages/add/Add'
+import {Route, Routes} from 'react-router-dom'
+import Orders from './pages/orders/Orders'
+import List from './pages/list/List'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr/>
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
