2b702d30b86d9f8eded95a4d692273e784315584
Glen Alloy <glenalloy8@gmail.com>
2024-10-25 21:29:32 +0530

    implementing user registration

--- food-delivery-backend/controllers/userController.js
+++ food-delivery-backend/controllers/userController.js
@@ -3,3 +3,43 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import validator from 'validator'
 
+// Login user
+const loginUser = async (req,res) => {
+
+}
+
+// Register user
+const registerUser = async (req,res) => {
+    const {name,password,email} = req.body;
+    try {
+        // Checking if user already exists
+        const exists = await userModel.findOne({email});
+        if (exists) {
+           return res.json({success:false,message:"User already exists"}) 
+        }
+        // validating email format & strong password
+        if (!validator.isEmail(email)) {
+           return res.json({success:false,message:"Please enter a valid Email"}) 
+        }
+
+        if (password.length<8) {
+           return res.json({success:false,message:"Please enter a strong password"}) 
+        }
+
+        // Hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+    } catch (error) {
+        
+    }
+}
+
+export {loginUser,registerUser}
--- food-delivery-backend/server.js
+++ food-delivery-backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 
 // app config
@@ -18,6 +19,7 @@ connectDB();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads')) //mounted the uploads folder into the /images endpoint
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res) => {
     res.send("API Working")
