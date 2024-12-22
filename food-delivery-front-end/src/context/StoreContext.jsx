f0638a9e28a091402b5aabb67bb07d55b5c9d11d
glen654 <glenalloy@gmail.com>
2024-12-22 19:58:06 +0530

    debugged the getotalcaramount function

--- food-delivery-front-end/src/context/StoreContext.jsx
+++ food-delivery-front-end/src/context/StoreContext.jsx
@@ -1,72 +1,75 @@
 import { createContext, useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
+  const [food_list, setFoodList] = useState([]);
 
-    const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000";
-    const [token,setToken] = useState("");
-    const [food_list,setFoodList] = useState([]); 
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
 
-    const addToCart = (itemId) => {
-        if(!cartItems[itemId]){
-            setCartItems((prev)=>({...prev,[itemId]:1}))
-        }else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find(
+          (product) => String(product.id) === String(item)
+        );
+        if (itemInfo) {
+          totalAmount += itemInfo.price * cartItems[item];
+        } else {
+          console.warn(`Item not found for id: ${item}`);
         }
+      }
     }
+    return totalAmount;
+  };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  const fetchFoodList = async () => {
+    const response = await axios.get(url + "/api/food/get");
+    setFoodList(response.data.data);
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      await fetchFoodList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+      }
     }
+    loadData();
+  }, []);
 
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for(const item in cartItems){
-            if(cartItems[item] > 0){
-                let itemInfo = food_list.find((product) => product.id === item);
-                totalAmount += itemInfo.price * cartItems[item];
-            }else {
-                console.warn(`Item not found for id: ${item}`);
-            }
-        }
-        return totalAmount;
-    }
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    url,
+    token,
+    setToken,
+  };
 
-    const fetchFoodList = async () => {
-        const response = await axios.get(url+"/api/food/get");
-        setFoodList(response.data.data)
-    }
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
-    useEffect(()=>{
-        async function loadData() {
-            await fetchFoodList();
-            if (localStorage.getItem("token")) {
-                setToken(localStorage.getItem("token"));
-            }
-        }
-        loadData();
-    },[])
-
-    const contextValue = {
-        food_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart,
-        getTotalCartAmount,
-        url,
-        token,
-        setToken
-    };
-    
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
-
-export default StoreContextProvider
+export default StoreContextProvider;
