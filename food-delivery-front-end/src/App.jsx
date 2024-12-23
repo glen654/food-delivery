9381eb6b022709b97e80078c0a202072f40e38b7
glen654 <glenalloy@gmail.com>
2024-12-23 21:47:11 +0530

    implemented verify payment functionality

--- food-delivery-front-end/src/App.jsx
+++ food-delivery-front-end/src/App.jsx
@@ -1,33 +1,31 @@
-import React, { useState } from 'react'
-import Navbar from './components/navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Cart from './pages/cart/Cart'
-import PlaceOrder from './pages/place-order/PlaceOrder'
-import Footer from './components/footer/Footer'
-import LoginPopUp from './components/loginPopUp/LoginPopUp'
-import Verify from './pages/verify/Verify'
+import React, { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import PlaceOrder from "./pages/place-order/PlaceOrder";
+import Footer from "./components/footer/Footer";
+import LoginPopUp from "./components/loginPopUp/LoginPopUp";
+import Verify from "./pages/verify/Verify";
 
 const App = () => {
-
-  const [showLogin,setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin}/>
+      {showLogin ? <LoginPopUp setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<PlaceOrder />} />
-          <Route path='/verify' element={<Verify/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </div>
       <Footer />
     </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
