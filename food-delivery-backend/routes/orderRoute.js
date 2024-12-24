9caa451f692b0a8408d96361b3ae3769e374d448
glen654 <glenalloy@gmail.com>
2024-12-24 11:27:05 +0530

    userOrders function implemented in order controller

--- food-delivery-backend/routes/orderRoute.js
+++ food-delivery-backend/routes/orderRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import {
+  placeOrder,
+  userOrders,
+  verifyOrder,
+} from "../controllers/orderController.js";
+import authMiddleWare from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
-orderRouter.post("/verify",verifyOrder);
+orderRouter.post("/verify", verifyOrder);
+orderRouter.post("/userorders", authMiddleWare, userOrders);
 export default orderRouter;
