2449316edf771b4d289742ebeda04e0d592a6fa0
glen654 <glenalloy@gmail.com>
2024-12-24 12:56:22 +0530

    implementing orders component on admin panel

--- admin/src/pages/orders/Orders.jsx
+++ admin/src/pages/orders/Orders.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './orders.css'
+import { useState } from 'react'
 
-const Orders = () => {
+const Orders = ({url}) => {
+  const [orders,setOrders] =  useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url+"/api/order/list");
+    if(response.data.success){
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    }else{
+      toast.error("Error");
+    }
+  }
   return (
-    <div>Orders</div>
+    <div>
+      
+    </div>
   )
 }
 
